42fed0636993456d80bb4d81b503cf73b3678bd8
william.wei <william.wei@yimlink.com>
2023-03-08 16:56:32 +0800

    [dev] bind port

--- src/main/java/com/dobbinsoft/netting/server/cluster/ClusterHeartBeatPusher.java
+++ src/main/java/com/dobbinsoft/netting/server/cluster/ClusterHeartBeatPusher.java
@@ -91,7 +91,7 @@ public class ClusterHeartBeatPusher {
             String data = datagramPacket.content().toString(StandardCharsets.UTF_8);
             ClusterNode clusterNode = JsonUtils.parse(data, ClusterNode.class);
             clusterNodeMapper.heartBeatCluster(clusterNode);
-            log.info("[Received UDP Data]");
+            log.info("[Received UDP Data] clusterNode=" + JsonUtils.toJson(clusterNode));
         }
     }
 
